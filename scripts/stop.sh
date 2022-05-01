ca8a308ee224e629d413ad324f02850d6544730e
Honda <hondao1207@gmail.com>
2022-05-02 01:45:17 +0900

    appspecfile

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# cd /home/ec2-user/etube-backend
-# nvm use 16
-# pm2 stop all
+cd /home/ec2-user
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm install node
+nvm install 16
+curl -o- -L https://yarnpkg.com/install.sh | bash
+cd /etube-backend
+yarn install
+yarn global add pm2
+pm2 stop all
