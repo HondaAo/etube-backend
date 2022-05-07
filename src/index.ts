e70ce26a9ed92c021b7be3b8a81ce19a78312d2d
Honda <hondao1207@gmail.com>
2022-05-07 15:29:03 +0900

    scripts change

--- src/index.ts
+++ src/index.ts
@@ -3,72 +3,20 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { createConnection } from "typeorm";
 import 'dotenv'
-import { User } from "./entity/User";
+// import { User } from "./entity/User";
 import { Video } from "./entity/Video";
 import { createSchema } from "./createSchema";
-import { Script } from "./entity/Script";
-
-// (async () => {
-//   const app = express();
-//   // const pool = new Pool({
-//   //   connectionString: process.env.DATABASE_URL,
-//   //   ssl: true
-//   // });
-//   // const RedisStore = connectRedis(session);
-//   // app.use(
-//   //   session({
-//   //     // store: new RedisStore({
-//   //     //   client: redis as any
-//   //     // }),
-//   //     name: "qid",
-//   //     secret: process.env.SESSION_SECRET || "aslkdfjoiq12312",
-//   //     resave: false,
-//   //     saveUninitialized: false,
-//   //     cookie: {
-//   //       httpOnly: true,
-//   //       secure: process.env.NODE_ENV === "production",
-//   //       maxAge: 1000 * 60 * 60 * 24 * 1 * 365
-//   //     }
-//   //   })
-//   // );
-//   //  await pool.connect()
-//   // const dbOptions = await getConnectionOptions(
-//   //   process.env.NODE_ENV || "development"
-//   // );
-//   await createConnection({
-//      name: "default",
-//      url: process.env.DATABASE_URL,
-//      type: 'postgres',
-//      entities: ["dist/entity/**/*.js"],
-//      synchronize: true,
-//   });
-//   app.use(cors())
-//   const apolloServer = new ApolloServer({
-//     schema: await buildSchema({
-//       resolvers: [BookResolver, VideoResolver],
-//       validate: false
-//     }),
-//     context: ({ req, res }) => ({ req, res }),
-//     introspection: true,
-//     playground: true,
-//   });
-
-//   apolloServer.applyMiddleware({ app, cors: false });
-//   const port = process.env.PORT || 4000;
-//   app.listen(port, () => {
-//     console.log(`server started at http://localhost:${port}/graphql`);
-//   });
-// })();
+// import { Script } from "./entity/Script";
 
 const main = async () => {
   await createConnection({
     type: "postgres",
-    database: "elearn",
-    username: "postgres",
-    password: "postgres",
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     logging: true,
     synchronize: true,
-    entities: [User, Script, Video],
+    entities: [Video],
   });
 
   const app = express();
